f23b63322eeb46d30ff58d7f75d80ab0e9465c25
CristianZelaya <criszel94@gmail.com>
2022-11-25 17:02:36 -0600

    Se agrego el panel de administracion

--- series/admin.py
+++ series/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.admin.decorators import register
+from .models import Serie, Episode
 
 # Register your models here.
+class SeriesAdmin( ModelAdmin ):
+    pass
+
+admin.site.register(Serie, SeriesAdmin)
+
+#class EpisodeAdmin( ModelAdmin ):
+    #pass
+
+#con decoradores
+@register( Episode )
+class EpisodeAdmin( ModelAdmin ):
+    pass
+
+#admin.site.register(Episode, EpisodeAdmin)
--- series/models.py
+++ series/models.py
@@ -5,7 +5,14 @@ class Serie( models.Model ):
     title = models.CharField(max_length=50)
     description = models.TextField()
 
+    #Se tradue en objeto a string
+    def __str__(self):
+        return self.title
+
 class Episode( models.Model ):
     number = models.IntegerField()
     name = models.CharField(max_length=50)
     serie = models.ForeignKey(Serie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Episode {self.number} - {self.name}'
